caf35864128ea5579707f08f855412d850b4ff63
Khanh Vuong Bui <bvkhanh0502@gmail.com>
2018-07-04 14:29:41 +0700

    using toastr for categoryController

--- BlogEngine.Web/Assets/Clients/js/controllers/categoryController.js
+++ BlogEngine.Web/Assets/Clients/js/controllers/categoryController.js
@@ -18,18 +18,17 @@
                     var treeConfig = {
                         data: data,
                         showTags: true,
-                        enableLinks: true,                        
-                        highlightSelected:false,
+                        enableLinks: true,
+                        highlightSelected: false,
                         levels: 1
                     };
-                    //$('#tree').attr('tags',[])
                     $('#tree').treeview(treeConfig);
                 } else {
-                    console.log(result.errorMessage);
+                    toastr.error(result.errorMessage, "Error");
                 }
             },
             error: function (xhr, status, error) {
-                console.log('Error:' + error);
+                toastr.error(result.errorMessage, "Error");
             }
         };
         $.ajax(ajaxConfig);
